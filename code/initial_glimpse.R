dc53496cd3a8b00342cfaa850643b55312c23ee1
Sam Lee Herring <samleeherring@gmail.com>
2024-06-28 12:30:27 -0700

    reorganized directory, finished initial glimpse and initial figures

--- code/initial_glimpse.R
+++ code/initial_glimpse.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(ggtext)
 
-raw_data <- read_csv('DBEDT_Data.csv')
+raw_data <- read_csv('data/DBEDT_Data.csv')
 
 just_pop <- raw_data %>%
   pivot_longer(cols= 4:87, names_to = 'year', values_to = 'population') %>%
@@ -48,12 +48,10 @@ pop_graph <- just_pop %>%
     plot.title = element_textbox_simple(margin = margin(b=5)),
     plot.subtitle = element_text(color = 'darkgrey', size = 10),
     panel.background = element_rect(fill = '#f6f6f6'),
-    #panel.grid = element_blank(),
     legend.background = element_rect(fill = "white"),
     legend.key = element_rect(fill = '#f6f6f6'),
     legend.position = "inside",
     legend.position.inside = c(0.19, 0.78),
-    #legend.text = element_text(color="black"),
     axis.line = element_line(),
     plot.margin = margin(10,5,10,5),
     panel.spacing = unit(0.3, 'in')
@@ -78,7 +76,6 @@ births_v_deaths <- just_pop %>%
   geom_line() +
   geom_point(size = 1) +
   geom_vline(xintercept = 2020, color = 'black', linetype = 'dashed', linewidth = 0.4) +
-  #geom_smooth(se=FALSE, linewidth = 0.3, color = 'black') +
   scale_y_continuous(breaks = seq(1000, 22000, 5000), labels = seq(1, 22, 5),
                      limits = c(1000, 22000)) +
   scale_x_continuous(breaks = seq(1958, 2023, 15), expand = c(0.01,0.01), limits = c(1957, 2024)) +
@@ -89,18 +86,16 @@ births_v_deaths <- just_pop %>%
     title = "Changes in population of state of Hawai'i (1958-2023)",
     subtitle = "Based on data from the Hawai'i State Dep of Health",
     x = NULL,
-    y = 'Popuklation change (thousands)'
+    y = 'Population change (thousands)'
   ) +
   theme(
     plot.title = element_textbox_simple(margin = margin(b=5)),
     plot.subtitle = element_text(color = 'darkgrey', size = 10),
     panel.background = element_rect(fill = '#f6f6f6'),
-    #panel.grid = element_blank(),
     legend.background = element_rect(fill = "white"),
     legend.key = element_rect(fill = '#f6f6f6'),
     legend.position = "inside",
     legend.position.inside = c(0.39, 0.48),
-    #legend.text = element_text(color="black"),
     axis.line = element_line(),
     plot.margin = margin(10,5,10,5),
     panel.spacing = unit(0.3, 'in')
@@ -114,3 +109,58 @@ births_v_deaths +
 
 ggsave('figures/births_v_deaths.png', width = 5, height = 4, units = 'in')
 
+## Looking at the population compared to amount of tourists any given year
+res_v_tourist <- raw_data %>%
+  pivot_longer(cols= 4:87, names_to = 'year', values_to = 'populations') %>%
+  select(1, 5, 6) %>%
+  pivot_wider(names_from = 'Indicator', values_from = 'populations') %>%
+  select(1,3,13) %>%
+  filter(year > 1998) %>%
+  mutate(year = as.numeric(year)) %>%
+  rename(residents = 'Total Resident Population',
+         visitors = 'Visitor arrivals, Total (air + cruise ship)')
+
+res_v_trst <- res_v_tourist %>%
+  pivot_longer(cols=2:3, names_to = 'populations', values_to = 'amounts') %>%
+  
+  ggplot(aes(x=year, y=amounts, group=populations, color=populations)) +
+  geom_point(size=1) +
+  geom_line(linewidth=0.75) +
+  geom_vline(xintercept = c(2001,2009,2020), color = 'black', linetype = 'dashed',
+             linewidth = 0.4) +
+  scale_y_continuous(breaks = seq(1200000, 11000000, 800000), labels = seq(120, 1100, 80),
+                     limits = c(1200000, 10600000)) +
+  scale_x_continuous(breaks = seq(1999, 2023, 3), expand = c(0.01,0.01), limits = c(1998, 2024)) +
+  scale_color_manual(name = 'Population type:',
+                     values=c("pink", "skyblue"),
+                     labels=c("Residents", "Tourists")) +
+  labs(
+    title = "Resident vs Tourist population of state of Hawai'i (1999-2023)",
+    subtitle = "Based on data from the Hawai'i State Dep Tourism & US Census",
+    x = NULL,
+    y = 'Populations (x100,000)'
+  ) +
+  theme(
+    plot.title = element_textbox_simple(margin = margin(b=10)),
+    plot.subtitle = element_text(color = 'darkgrey', size = 10),
+    panel.background = element_rect(fill = '#f6f6f6'),
+    legend.background = element_rect(fill = "white"),
+    legend.key = element_rect(fill = '#f6f6f6'),
+    legend.position = "inside",
+    legend.position.inside = c(0.26, 0.82),
+    axis.line = element_line(),
+    plot.margin = margin(10,5,10,5),
+    panel.spacing = unit(0.3, 'in')
+  )
+
+lbl_txt_3 <- data.frame(year = (2020 - 2), amounts =2000000, label = '(COVID-19)')
+lbl_txt_4 <- data.frame(year = c(2002, 2011.5), amounts=5200000,
+                        label = c('(9-11)', '(Housing crisis)'))
+
+res_v_trst +  
+  geom_text(data = lbl_txt_3 ,label = lbl_txt_3$label, color = 'black', size = 3, 
+            group = 1) +
+  geom_text(data = lbl_txt_4 ,label = lbl_txt_4$label, color = 'black', size = 3, 
+            group = 1)
+
+ggsave('figures/residents_v_tourists.png', width = 5, height = 4, units = 'in')
